e3c02351703ef15e473a6330550e90f08244381f
SabaShaikh09 <sabashaikh9257@gmail.com>
2022-01-24 16:29:58 +0530

    my first commit on reactcrudapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+//import ShowStudent from './ShowStudentComponent/ShowStudent';
+//import { StudOpreation } from './StudentData/StudentInterface';
+
+//import ShowStudent from './ShowStudentComponent/ShowStudent';
+
+//let studImpl=new StudOpreation();
+
+import ShowStudents from './Components/ShowStudent';
+import DeleteStudent from './Components/DeleteStudent';
+import Navbar from './NavbarComponent/Navbar';
+import {Route, Routes} from 'react-router-dom';
+import AddStudent from './Components/AddStudent';
+import UpdateStudent from './Components/UpdateStudent';
+
+import Footer from './Footer/Footer';
+// import ButtonAppBar from './NavBarComponent/Nav';
+
 
 function App() {
-  return (
+   
+  // let deleteStud=()=>{
+  //   studImpl.deleteStudent(101);
+  // }
+
+  // let showStud=()=>{
+  //   studImpl.getAllStudent();
+
+  // }
+
+  // return (
+  //   <div className="App">
+  //    {/* <button onClick={showStud}>show student</button>
+  //    <button onClick={deleteStud}>Test Delete</button> */}
+  //    <ShowStudent></ShowStudent>
+     
+  //   </div>
+  // );
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+         <Navbar></Navbar> 
+      </header> 
+     
+
+     
+      <Routes>
+        <Route path="/students/showall" element={<ShowStudents></ShowStudents>}></Route>
+        <Route path="/students/delete/:id" element={<DeleteStudent></DeleteStudent>}></Route> 
+        <Route path="/students/add" element={<AddStudent></AddStudent>}></Route>
+        <Route path="/students/update/:id"  element={<UpdateStudent></UpdateStudent>}></Route>
+        
+      </Routes>
+      
+      <div className="Foot">
+         <Footer></Footer>
+      </div>
     </div>
   );
+
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import finalStore from './store/finalStore';
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={finalStore}>
     <App />
+    </Provider>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
